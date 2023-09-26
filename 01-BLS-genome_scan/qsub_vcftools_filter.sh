1d279b1c1476f779a7007e3d1ec293ca1ed2c4ba
gabrielluishernandez <glhernandezg@unal.edu.co>
2023-09-26 14:52:59 +0100

    filter_vcf

---
+++ 01-BLS-genome_scan/qsub_vcftools_filter.sh
@@ -0,0 +1,10 @@
+mp 1
+#$ -l h_vmem=20G
+#$ -l h_rt=240:0:0
+#$ -cwd
+#$ -j y
+
+module load vcftools
+module load samtools
+
+vcftools --gzvcf input/2022-02-16-solenopsis_all_369samples.norm.maskfilter.biallelicsnps.DP1.GQ20.vcf.gz --keep input/s_invicta_samples_roddy --recode --stdout | bgzip -c > tmp/2022-03-15-Only_241_Inv.vcf.gz
